ee371a31e67e3eaa588135f9fe285a7c59bd7df6
Eric Fritz <eric@eric-fritz.com>
2020-01-08 13:53:32 -0600

    Update badge definitions.

--- template/src/extension.ts
+++ template/src/extension.ts
@@ -2,6 +2,7 @@ import { Handler, initLSIF } from '../../package/lib'
 import * as sourcegraph from 'sourcegraph'
 import { languageSpecs } from '../../languages'
 import { documentSelector } from '../../package/lib/handler'
+import { impreciseBadge } from './badges'
 
 const DUMMY_CTX = { subscriptions: { add: (_unsubscribable: any) => void 0 } }
 
@@ -9,22 +10,6 @@ const DUMMY_CTX = { subscriptions: { add: (_unsubscribable: any) => void 0 } }
 // within a file but different from other files.
 const file = (loc: sourcegraph.Location) =>
     `${loc.uri.host} ${loc.uri.pathname} ${loc.uri.hash}`
-
-// TODO
-/** circled question mark icons */
-const whiteBadge = `data:image/svg+xml;base64,${btoa(
-    `<svg xmlns='http://www.w3.org/2000/svg' viewBox="0 0 24 24" fill="#ffffff"><path d="M11,18H13V16H11V18M12,2C6.48,2 2,6.48 2,12C2,17.52 6.48,22 12,22C17.52,22 22,17.52 22,12C22,6.48 17.52,2 12,2M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,6C9.79,6 8,7.79 8,10H10C10,8.9 10.9,8 12,8C13.1,8 14,8.9 14,10C14,12 11,11.75 11,15H13C13,12.75 16,12.5 16,10C16,7.79 14.21,6 12,6Z"></path></svg>`
-)}`
-const blackBadge = `data:image/svg+xml;base64,${btoa(
-    `<svg xmlns='http://www.w3.org/2000/svg' viewBox="0 0 24 24" fill="#000000"><path d="M11,18H13V16H11V18M12,2C6.48,2 2,6.48 2,12C2,17.52 6.48,22 12,22C17.52,22 22,17.52 22,12C22,6.48 17.52,2 12,2M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,6C9.79,6 8,7.79 8,10H10C10,8.9 10.9,8 12,8C13.1,8 14,8.9 14,10C14,12 11,11.75 11,15H13C13,12.75 16,12.5 16,10C16,7.79 14.21,6 12,6Z"></path></svg>`
-)}`
-
-// TODO
-const badge = {
-    icon: whiteBadge,
-    hoverMessage: '...Heuristic...', // TODO
-    light: { icon: blackBadge },
-}
 
 export function activate(ctx: sourcegraph.ExtensionContext = DUMMY_CTX): void {
     // This is set to an individual language ID by the generator script.
@@ -52,7 +37,7 @@ export function activate(ctx: sourcegraph.ExtensionContext = DUMMY_CTX): void {
                         return undefined
                     }
 
-                    return { ...val, badge }
+                    return { ...val, badge: impreciseBadge }
                 },
             })
         )
@@ -69,7 +54,7 @@ export function activate(ctx: sourcegraph.ExtensionContext = DUMMY_CTX): void {
                         return undefined
                     }
 
-                    return val.map(v => ({ ...v, badge }))
+                    return val.map(v => ({ ...v, badge: impreciseBadge }))
                 },
             })
         )
@@ -91,7 +76,10 @@ export function activate(ctx: sourcegraph.ExtensionContext = DUMMY_CTX): void {
 
                     return [
                         ...lsifValues,
-                        ...searchReferences.map(v => ({ ...v, badge })),
+                        ...searchReferences.map(v => ({
+                            ...v,
+                            badge: impreciseBadge,
+                        })),
                     ]
                 },
             })
