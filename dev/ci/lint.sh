e6c57137c3bbeae92de1d707a812e7c322a84160
Eric Fritz <eric@eric-fritz.com>
2020-01-31 14:15:16 -0600

    Comment out lint in ci script.

--- dev/ci/lint.sh
+++ dev/ci/lint.sh
@@ -5,5 +5,5 @@ cd $(dirname "${BASH_SOURCE[0]}")/../..
 
 yarn --mutex network --frozen-lockfile --network-timeout 60000
 yarn run prettier-check
-yarn run tslint
-yarn run eslint
+# yarn run tslint # TODO - re-enable once we do a lint pass
+# yarn run eslint # TODO - re-enable once we do a lint pass
