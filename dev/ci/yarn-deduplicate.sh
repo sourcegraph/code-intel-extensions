e19bb1a678cfb248756cb57bb4fdfcece4bcf7db
Eric Fritz <eric@eric-fritz.com>
2020-01-31 14:55:41 -0600

    Disable deduplicate check in CI for now.

--- dev/ci/yarn-deduplicate.sh
+++ dev/ci/yarn-deduplicate.sh
@@ -3,7 +3,8 @@
 set -ex
 cd $(dirname "${BASH_SOURCE[0]}")
 
-./yarn-run.sh "yarn-deduplicate --fail --list --strategy fewer ./yarn.lock" || {
-    echo 'yarn.lock contains duplicate dependencies. Please run `yarn deduplicate` and commit the result.'
-    exit 1
-}
+# Disabled in CI for now as we require explicit resolutions
+# ./yarn-run.sh "yarn-deduplicate --fail --list --strategy fewer ./yarn.lock" || {
+#     echo 'yarn.lock contains duplicate dependencies. Please run `yarn deduplicate` and commit the result.'
+#     exit 1
+# }
