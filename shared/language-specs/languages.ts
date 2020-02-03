508b32e44902b112d31dea7b680ee03d7a5c5180
Chris Wendt <chrismwendt@gmail.com>
2020-02-03 14:55:16 -0700

    add slash to recognized characters

--- shared/language-specs/languages.ts
+++ shared/language-specs/languages.ts
@@ -446,7 +446,7 @@ export const languageSpecs: LanguageSpec[] = [
             languageID: 'clojure',
             fileExts: ['clj', 'cljs', 'cljx'],
             commentStyle: lispStyle,
-            identCharPattern: /[A-Za-z0-9_\-!?+*<>=]/,
+            identCharPattern: /[A-Za-z0-9_\-!?+*<>=/]/,
         },
         stylized: 'Clojure',
     },
