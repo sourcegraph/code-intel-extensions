98abb46b88cf11ffad9d3304ed939666884a7246
Eric Fritz <eric@eric-fritz.com>
2020-02-18 15:50:09 -0600

    Fix bad yargs usage.

--- dev/scripts/args.ts
+++ dev/scripts/args.ts
@@ -9,17 +9,17 @@ export function findLanguageSpecs(): LanguageSpec[] {
         .nargs('languages', 1)
         .describe('l', 'A list of (comma-separated) languages to generate')
         .alias('l', 'languages')
-        .strict().argv as { language?: string }
+        .strict().argv as { languages?: string }
 
     const candidates = languageSpecs.filter(
         s => !blacklist.includes(s.languageID)
     )
 
-    if (!args.language) {
+    if (!args.languages) {
         return candidates
     }
 
-    return args.language.split(',').map(languageID => {
+    return args.languages.split(',').map(languageID => {
         const spec = candidates.find(spec => spec.languageID === languageID)
         if (!spec) {
             throw new Error(`Unknown language ${languageID}.`)
