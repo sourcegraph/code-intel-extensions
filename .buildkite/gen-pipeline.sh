7d525b74a2b8dabda1783235729f4a1881214d4a
Eric Fritz <eric@eric-fritz.com>
2020-02-25 20:51:52 -0600

    Publish extensions on master branch.

---
+++ .buildkite/gen-pipeline.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -e
+cd $(dirname "${BASH_SOURCE[0]}")/..
+
+# Set icons for languages that do not have a buildkite emoji
+# https://github.com/buildkite/emojis
+declare -A special_icons
+special_icons=(
+    ["cuda"]="question"
+    ["dart"]="dartlang"
+    ["groovy"]="question"
+    ["lisp"]="question"
+    ["ocaml"]="question"
+    ["pascal"]="question"
+    ["perl"]="question"
+    ["powershell"]="question"
+    ["protobuf"]="question"
+    ["r"]="question"
+    ["verilog"]="question"
+    ["vhdl"]="question"
+)
+
+tmpfile=$(mktemp /tmp/gen-pipeline.XXXXXX)
+trap "{ rm -f $tmpfile; }" EXIT
+
+# Determine the languages we can generate from the icons present
+# in the repo. We have a test to ensure each defined language has
+# a png file with the same basename as the language identifier.
+# Strip the suffix of each icon in the list.
+
+icons=($(ls icons))
+langs=(`echo ${icons[@]%.*}`)
+
+for lang in ${langs[@]}; do
+    # Skip lsp-backed extensions
+    if [ "${lang}" = "go" ] || [ "${lang}" = "typescript" ]; then
+        continue
+    fi
+
+cat << EOF >> ${tmpfile}
+  - command: ./.buildkite/gen-and-publish.sh "${lang}"
+    label: ':${special_icons[$lang]:-$lang}: :rocket:'
+    branches: master
+EOF
+done
+
+cat ./.buildkite/base-pipeline.yml ${tmpfile}
--- .buildkite/yarn-run.sh
+++ .buildkite/yarn-run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 set -ex
-cd $(dirname "${BASH_SOURCE[0]}")/../..
+cd $(dirname "${BASH_SOURCE[0]}")/..
 
 # mutex is necessary since CI runs various yarn installs in parallel
 yarn --mutex network --frozen-lockfile --network-timeout 60000
 
 for cmd in "$@"; do
-    yarn -s run $cmd
+    yarn -s --cwd "${CWD:-.}" run ${cmd}
 done
