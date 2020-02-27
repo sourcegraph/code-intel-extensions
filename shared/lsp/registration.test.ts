79a95c739a614ac5e432003445693206434774ef
Loïc Guychard <loic@sourcegraph.com>
2020-02-27 16:00:56 +0100

    doh

--- shared/lsp/registration.test.ts
+++ shared/lsp/registration.test.ts
@@ -3,9 +3,9 @@ import { scopeDocumentSelectorToRoot } from './registration'
 
 describe('scopeDocumentSelectorToRoot()', () => {
     it('builds selectors from clientRootUri', () => {
-        assert.equal(
+        assert.deepStrictEqual(
             scopeDocumentSelectorToRoot(
-                [],
+                ['l'],
                 new URL(
                     'git://github.com/gorilla/mux?d83b6ffe499a29cc05fc977988d0392851779620'
                 )
@@ -20,7 +20,7 @@ describe('scopeDocumentSelectorToRoot()', () => {
         )
     })
     it('builds selectors from clientRootUri and pattern', () => {
-        assert.equal(
+        assert.deepStrictEqual(
             scopeDocumentSelectorToRoot(
                 [{ pattern: '*.go' }],
                 new URL(
