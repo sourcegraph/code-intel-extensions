ad7f65bb61fc50e15d24129880648468d636c83e
Eric Fritz <eric@sourcegraph.com>
2020-02-27 09:22:45 -0600

    Fix document selector scope (#265)
    * Fix document selector scope
    
    * Shorten lines.
    
    * Fix tests.
    
    * Add foo.
    
    * Remove foo.
    
    * tests
    
    * doh
    
    * Style fixes.
    
    Co-authored-by: Loïc Guychard <l.guychard@gmail.com>

--- shared/lsp/registration.ts
+++ shared/lsp/registration.ts
@@ -607,7 +607,11 @@ export function scopeDocumentSelectorToRoot(
         )
         .map(filter => ({
             ...filter,
-            // TODO filter.pattern needs to be run resolved relative to server root URI before mounting on clientRootUri
-            pattern: new URL(filter.pattern ?? '**', clientRootUri).href,
+            // TODO filter.pattern needs to be run resolved relative to server root URI before
+            // mounting on clientRootUri
+
+            // Root URIs are of the form git://repo?commit-sha, so new URL(pattern, URI) would
+            // strip the query string, resulting in a pattern that matches any commit ID.
+            pattern: `${clientRootUri.href}#${filter.pattern ?? '**/**'}`,
         }))
 }
