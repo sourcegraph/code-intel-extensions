4b2239f6e617b0768d91f25055aa0a01fb22a477
Eric Fritz <eric@eric-fritz.com>
2021-09-14 09:46:49 -0500

    Ensure CI runs lint and tests.

--- .buildkite/lint.sh
+++ .buildkite/lint.sh
@@ -4,4 +4,6 @@ set -ex
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 yarn --mutex network --frozen-lockfile --network-timeout 60000
-yarn run build eslint prettier-check
+yarn run build
+yarn run eslint
+yarn run prettier-check
--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -4,4 +4,6 @@ set -ex
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 yarn --mutex network --frozen-lockfile --network-timeout 60000
-yarn run build test coverage
+yarn run build
+yarn run test
+yarn run coverage
