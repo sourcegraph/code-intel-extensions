89ba23a364fc7b9e0728aea7fbbb9ba78a653eb9
Eric Fritz <eric@eric-fritz.com>
2022-01-10 16:24:19 -0600

    Update test.

--- template/src/search/queries.test.ts
+++ template/src/search/queries.test.ts
@@ -23,6 +23,7 @@ describe('search requests', () => {
                     '^token$',
                     'type:symbol',
                     'patternType:regexp',
+                    'count:500',
                     'case:yes',
                     'file:\\.(cpp)$',
                 ],
